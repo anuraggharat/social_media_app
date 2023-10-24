f33c8b18a696d3489b5afe9d826aeb4bc7462b17
Anurag Gharat <anuraggharat55@gmail.com>
2023-10-24 11:31:51 +0530

    users section added

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -33,28 +33,13 @@ const UserBox: React.FC<UserBoxProps> = ({
       {isLoading && (
         <LoadingModal />
       )}
-      <div
-        onClick={handleClick}
-        className="
-          w-full 
-          relative 
-          flex 
-          items-center 
-          space-x-3 
-          bg-white 
-          p-3 
-          hover:bg-neutral-100
-          rounded-lg
-          transition
-          cursor-pointer
-        "
-      >
+      <div onClick={handleClick} className=" w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 dark:bg-neutral-950 dark:hover:bg-neutral-900 rounded-lg transition cursor-pointer " >
         <Avatar user={data} />
         <div className="min-w-0 flex-1">
           <div className="focus:outline-none">
             <span className="absolute inset-0" aria-hidden="true" />
             <div className="flex justify-between items-center mb-1">
-              <p className="text-sm font-medium text-gray-900">
+              <p className="text-sm font-medium text-gray-900 dark:text-neutral-200">
                 {data.name}
               </p>
             </div>
--- app/users/components/UserList.tsx
+++ app/users/components/UserList.tsx
@@ -8,19 +8,8 @@ interface UserListProps {
 
 export default function UserList({items}:UserListProps) {
   return (
-    <div className="
-    fixed 
-    inset-y-0 
-    pb-20
-    lg:pb-0
-    lg:left-20 
-    lg:w-80 
-    lg:block
-    overflow-y-auto 
-    border-r 
-    border-gray-200
-    block w-full left-0
-  ">
+    <div className=" fixed inset-y-0 pb-20 lg:pb-0 lg:left-24 lg:w-80 lg:block overflow-y-auto border-r 
+    border-gray-200 block w-full left-0 dark:bg-neutral-950 dark:border-neutral-500">
   <div className="px-5">
         <div className="flex-col">
           <div 
@@ -29,6 +18,7 @@ export default function UserList({items}:UserListProps) {
               font-bold 
               text-neutral-800 
               py-4
+              dark:text-neutral-200
             "
           >
             People
